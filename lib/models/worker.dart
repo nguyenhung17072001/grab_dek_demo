13716f8a3980d91571a08bdd32269592ef69c0a6
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-07 17:19:52 +0700

    up

---
+++ lib/models/worker.dart
@@ -0,0 +1,11 @@
+
+
+class Worker {
+  final String name;
+  final String lat;
+  final String lon;
+
+  Worker({required this.name, required this.lat, required this.lon});
+
+  
+}
--- lib/screens/host_map.dart
+++ lib/screens/host_map.dart
@@ -1,33 +1,152 @@
 import 'dart:async';
-import 'dart:ffi';
 
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-
-
+import 'package:grab_dek_demo/models/worker.dart';
+import 'package:marker_icon/marker_icon.dart';
 
 class HostMap extends StatefulWidget {
   final double latitude;
   final double longitude;
-  HostMap({super.key, required this.latitude, required this.longitude});
+  HostMap({Key? key, required this.latitude, required this.longitude})
+      : super(key: key);
+
   @override
   State<HostMap> createState() => HostMapState();
 }
 
 class HostMapState extends State<HostMap> {
+  late double _latitude;
+  late double _longitude;
+  late CameraPosition _kGooglePlex;
+  late BitmapDescriptor hostIcon;
+  
+  static const List<dynamic> wokers = [
+    {
+      "name": "Thợ 1",
+      "lat": "21.0154722",
+      "lon": "105.855"
+    },
+    {
+      "name": "Thợ 2",
+      "lat": "21.014599",
+      "lon": "105.851642"
+    },
+    {
+      "name": "Thợ 3",
+      "lat": "21.015922",
+      "lon": "105.859339"
+    },
+    {
+      "name": "Thợ 4",
+      "lat": "21.017581",
+      "lon": "105.855529"
+    },
+    {
+      "name": "Thợ 5",
+      "lat": "21.009929716805722",
+      "lon": "105.84106206806747"
+    },
+    {
+      "name": "Thợ 6",
+      "lat": "21.009929716805722",
+      "lon": "105.84106206806747"
+    },
+    {
+      "name": "Thợ 7",
+      "lat": "21.014211855697347",
+      "lon": "105.84902786667328"
+    },
+    {
+      "name": "Thợ 8",
+      "lat": "20.983579",
+      "lon": "105.849482"
+    },
+    {
+      "name": "Thợ 9",
+      "lat": "20.988025",
+      "lon": "105.841313"
+    },
+    {
+      "name": "Thợ 10",
+      "lat": "21.000263",
+      "lon": "105.832091"
+    },
+
+  ];
+ 
+
+  
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+   
+    _latitude = widget.latitude;
+    _longitude = widget.longitude;
+    _kGooglePlex = CameraPosition(
+      target: LatLng(_latitude, _longitude),
+      zoom: 15,
+    );
+   
+  
+    super.initState();
+  }
+
+  void _addHostMarker() async {
+    _markers.add(
+      Marker(
+        markerId: const MarkerId("kGooglePlexMarker"),
+        position: LatLng(_latitude, _longitude),
+        icon: await MarkerIcon.pictureAsset(
+          assetPath: 'lib/assets/icons/house_icon.png',
+          width: 120,
+          height: 120,
+        ),
+        infoWindow: const InfoWindow(
+          title: "Chủ nhà",
+          snippet: "Chủ nhà đi tìm thợ",
+        ),
+      ),
+    );
+    setState(() {
+
+    });
+  }
+  void _addWorkerMarkers(List<dynamic> workers) async {
+  for (var worker in workers) {
+    double lat = double.parse(worker['lat'].toString());
+    double lon = double.parse(worker['lon'].toString());
+
+    _markers.add(
+      Marker(
+        markerId: MarkerId(worker['name']),
+        position: LatLng(lat, lon),
+        icon: await MarkerIcon.pictureAsset(
+          assetPath: 'lib/assets/icons/worker_icon.png', // Change to your worker icon path
+          width: 120,
+          height: 120,
+        ),
+        infoWindow: InfoWindow(
+          title: worker['name'],
+          snippet: "Worker's details here", // You can add worker-specific information here
+        ),
+      ),
+    );
+  }
+  setState(() {});
+}
+
+
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
   static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      zoom: 19.151926040649414
+    bearing: 192.8334901395799,
+    target: LatLng(37.43296265331129, -122.08832357078792),
+    zoom: 19.151926040649414,
   );
 
   @override
@@ -37,11 +156,17 @@ class HostMapState extends State<HostMap> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: GoogleMap(
-          mapType: MapType.hybrid,
+          mapType: MapType.normal,
           initialCameraPosition: _kGooglePlex,
-          onMapCreated: (GoogleMapController controller) {
+          onMapCreated: (GoogleMapController controller) async {
             _controller.complete(controller);
+            _addHostMarker(); 
+            _addWorkerMarkers(wokers);
+
+            
+            //_addWorkerMarkers(workers);
           },
+          markers: _markers,
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -56,4 +181,7 @@ class HostMapState extends State<HostMap> {
     final GoogleMapController controller = await _controller.future;
     await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
+
+
+  
 }
