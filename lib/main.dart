790fddd2ce5f3f0da662a3d1e465261b91dd270a
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-07 11:04:28 +0700

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,64 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 
 import 'navigation/bottom_tab.dart';
-
+import 'package:geolocator/geolocator.dart';
 
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<void> _requestPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+
+      bool openSettings = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Yêu cầu quyền vị trí'),
+            content: const Text('Ứng dụng cần truy cập vị trí để hoạt động.'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Đồng ý'),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+              TextButton(
+                child: const Text('Cài đặt'),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+            ],
+          );
+        },
+      );
+
+      if (openSettings) {
+        await Geolocator.openAppSettings();
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _requestPermission();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -19,5 +66,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
--- lib/screens/host_screen.dart
+++ lib/screens/host_screen.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
 import 'package:grab_dek_demo/screens/map_sample.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -13,27 +16,72 @@ class HostScreen extends StatefulWidget {
 
 class _HostScreenState extends State<HostScreen> {
   final myController = TextEditingController();
+  final TextEditingController _addressValue = TextEditingController();
+  TextEditingController _addressValue2 = TextEditingController();
+
+  late Position _currentPosition;
+  late String _currentAddress;
 
   @override
   void initState() {
     super.initState();
-
     // Start listening to changes.
-    myController.addListener(_printLatestValue);
+    _getCurrentLocation();
+
+    _addressValue.addListener(_printLatestValue);
+  }
+
+  void _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _currentPosition = position;
+    });
+    var url = Uri.https('nominatim.openstreetmap.org', '/reverse', {
+      'format': 'json',
+      'lat': position.latitude.toString(), // Chuyển latitude thành chuỗi
+      'lon': position.longitude.toString(), // Chuyển longitude thành chuỗi
+    });
+    var response = await http.get(url);
+    try {
+      if (response.statusCode == 200) {
+        var decodedBody =
+            convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+        if (decodedBody.containsKey('address')) {
+          var addressData = decodedBody['address'] as Map<String, dynamic>;
+
+          // Accessing nested fields with proper error handling
+          String quarter = addressData['quarter'] ?? '';
+          String suburb = addressData['suburb'] ?? '';
+          String city = addressData['city'] ?? '';
+
+          setState(() {
+            _addressValue.text = '$quarter, $suburb $city';
+          });
+          print('Address: ${_addressValue.text}');
+        } else {
+          print('Address data not found in the response.');
+        }
+      } else {
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      print('sssssssssssssssssssssssss: $e');
+    }
   }
 
   @override
   void dispose() {
-    // Clean up the controller when the widget is removed from the widget tree.
-    // This also removes the _printLatestValue listener.
     myController.dispose();
     super.dispose();
   }
 
   void _printLatestValue() {
-    final text = myController.text;
-    print('Second text field: $text (${text.characters.length})');
+    final text = _addressValue.text;
+    print(text);
   }
+
   File? _image;
 
   final ImagePicker _picker = ImagePicker();
@@ -49,62 +97,56 @@ class _HostScreenState extends State<HostScreen> {
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       
-       title: Text('Chủ nhà'), 
+        title: const Text('Chủ nhà'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: getImage, // Gọi hàm để chọn ảnh
         tooltip: 'Chọn ảnh',
-        child: Icon(Icons.add_a_photo),
+        child: const Icon(Icons.add_a_photo),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(10, 15, 10, 0),
-            child: TextField(
-              decoration: const InputDecoration(
-                labelText: 'Địa chỉ',
-                border: OutlineInputBorder(),
-                
-              ),
-              onChanged: (text) {
-                print('First text field: $text (${text.characters.length})');
-              },
+      body: Column(children: [
+        Container(
+          margin: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+          child: TextField(
+            controller: _addressValue,
+            decoration: const InputDecoration(
+              labelText: 'Địa chỉ',
+              border: OutlineInputBorder(),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.fromLTRB(10, 15, 10, 0),
-            child: TextField(
-              decoration: const InputDecoration(
-                labelText: 'Địa chỉ',
-                border: OutlineInputBorder(),
-                
-              ),
-              onChanged: (text) {
-                print('First text field: $text (${text.characters.length})');
-              },
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+          child: TextField(
+            decoration: const InputDecoration(
+              labelText: 'Tên công trình',
+              border: OutlineInputBorder(),
             ),
+            onChanged: (text) {
+              print('First text field: $text (${text.characters.length})');
+            },
           ),
-          _image == null
+        ),
+        _image == null
             ? const Text('Không có ảnh được chọn')
             : Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Image.file(
-                  _image!,
-                  fit: BoxFit.cover,
-                )
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Image.file(
+                      _image!,
+                      fit: BoxFit.cover,
+                    )),
               ),
-            ),
-          Container(
-            width: 200,
-            child: TextButton(
+        Container(
+          width: 200,
+          child: TextButton(
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
                 foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
@@ -115,11 +157,9 @@ class _HostScreenState extends State<HostScreen> {
                   MaterialPageRoute(builder: (context) => const MapSample()),
                 );
               },
-              child: Text('Tìm thợ')
-            ),
-          )
-          
-        ]),
+              child: Text('Tìm thợ')),
+        )
+      ]),
     );
   }
 }
