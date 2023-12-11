e136bdea0457b3910c1f9af042f8f901a81783a8
Nguyễn Hưng <74828782+nguyenhung17072001@users.noreply.github.com>
2023-12-11 15:06:02 +0700

    up

--- lib/screens/homeowner.dart
+++ lib/screens/homeowner.dart
@@ -15,18 +15,34 @@ class _HomeownerState extends State<Homeowner> {
     return Scaffold(
       body: Scaffold(
         appBar: AppBar(
-          backgroundColor: AppColors.primaryColor,
-          title: const Text('Chủ nhà tìm thợ'),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: const Text(
+            'Tìm thợ thi công',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Roboto-Bold',
+              fontSize: 14,
+            ),
+          ),
+          elevation: 1,
+          shadowColor: const Color(0xffF0F0F0),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop(); // Quay lại trang trước
+            },
+            color: Colors.black,
+          ),
           actions: [],
         ),
         body: Stack(
-          
           children: <Widget>[
-             PositionedCircle(
-              top: -40,
-              left: -40,
-              width: 180,
-              height: 180,
+            PositionedCircle(
+              top: -20,
+              left: -20,
+              width: 120,
+              height: 120,
             ),
             PositionedCircle(
               right: -40,
@@ -34,8 +50,55 @@ class _HomeownerState extends State<Homeowner> {
               width: 180,
               height: 180,
             ),
-
-
+            Positioned.fill(
+              child: Column(mainAxisSize: MainAxisSize.max, children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: const Text(
+                    'Thông tin chủ nhà',
+                    style: TextStyle(
+                      fontFamily: 'Roboto-Regular',
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 16,
+                        width: 16,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xffFFF100),
+                              Color(0xffFFB57E),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 2,
+                        width: 130,
+                        color: Color(0xffE6E6E6),
+                      ),
+                      Container(
+                        height: 16,
+                        width: 16,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xffE6E6E6),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ]),
+            )
           ],
         ),
       ),
